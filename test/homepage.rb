e83f9a6811438325e14a974ecd2083dba373786e
Dan Young <dan@ejecteject.co.uk>
2016-04-17 14:30:21 +0100

    added require capybara-webkit

--- test/homepage.rb
+++ test/homepage.rb
@@ -1,11 +1,12 @@
 require 'capybara'
+require 'capybara-webkit'
 
 session = Capybara::Session.new(:webkit)
-session.visit "http://www-dev.enginerbetter.com"
+session.visit "http://www-dev.engineerbetter.com"
 
-if session.has_content?("Deliver your core value, faster")
+if session.has_content?("Cloud Foundry")
   puts "EB Tagline found!"
 else
   puts "No tagline found :("
-  exit(-1)
+  exit( -1)
 end
