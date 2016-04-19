72071d098f76b3c33506a6189cf48de723c3591e
Dan Young <dan@ejecteject.co.uk>
2016-04-19 17:05:32 +0100

    use environment var for external URL in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,10 @@ require 'capybara-webkit'
 require 'capybara/rspec'
 
 Capybara.configure do |config|
+
+  environment = ENV.fetch("ENVIRONMENT")
+
   config.default_driver = :webkit
   config.run_server = false
-  config.app_host   = 'http://www-dev.engineerbetter.com'
+  config.app_host   = "http://www-#{environment}.engineerbetter.com"
 end
