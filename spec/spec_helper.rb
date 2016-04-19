b4580bd9ce66309d3ebb57b3b317e2f09f992ce3
Dan Young <dan@ejecteject.co.uk>
2016-04-19 12:42:45 +0100

    added new smoketest for homepage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,9 @@
+require 'capybara'
+require 'capybara-webkit'
 require 'capybara/rspec'
+
+Capybara.configure do |config|
+  config.current_driver = :webkit
+  config.run_server = false
+  config.app_host   = 'http://www-dev.engineerbetter.com'
+end
