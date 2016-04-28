1b4771784ad408ce34bfd51911f22d22237e8be2
Dan Young <dan.young@engineerbetter.com>
2016-04-28 09:38:56 +0100

    update smoketest for new values copy

--- spec/navi_acceptance_spec.rb
+++ spec/navi_acceptance_spec.rb
@@ -4,7 +4,7 @@ feature "Basic navigation" do
   scenario "Go from Values, to CF, to Rates" do
     visit '/'
     click_link 'Our Values'
-    expect(page).to have_content 'OUR VALUES DRIVE US'
+    expect(page).to have_content 'Our Values Drive Us'
 
     click_link 'Cloud Foundry', match: :first
     expect(page).to have_content 'We offer holistic services'
