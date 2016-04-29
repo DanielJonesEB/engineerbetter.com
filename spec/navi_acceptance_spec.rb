a65a49d19ea13e696e31a4294ce20b1cf8777542
Dan Young <dan.young@engineerbetter.com>
2016-04-29 15:38:40 +0100

    update smoketest to match first instance of Our Values

--- spec/navi_acceptance_spec.rb
+++ spec/navi_acceptance_spec.rb
@@ -3,7 +3,7 @@ feature "Basic navigation" do
 
   scenario "Go from Values, to CF, to Rates" do
     visit '/'
-    click_link 'Our Values' match: :first
+    click_link 'Our Values', match: :first
     expect(page).to have_content 'Our Values Drive Us'
 
     click_link 'Cloud Foundry', match: :first
