a4244bc2dd7f2af3872b0d4bfb22c1e8255967f7
Daniel Jones <daniel.jones@engineerbetter.com>
2016-09-21 09:00:49 +0100

    case sensitivity in spec

--- spec/navi_acceptance_spec.rb
+++ spec/navi_acceptance_spec.rb
@@ -4,7 +4,7 @@ feature "Basic navigation" do
   scenario "Go from Values, to CF, to Rates" do
     visit '/'
     click_link 'How We Work', match: :first
-    expect(page).to have_content 'Our Values'
+    expect(page).to have_content 'Our values'
 
     click_link 'Cloud Foundry', match: :first
     expect(page).to have_content 'We offer holistic services'
