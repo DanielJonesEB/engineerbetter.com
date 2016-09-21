649abe0849890a9a715a31032cc97937a67b4b28
Daniel Jones <daniel.jones@engineerbetter.com>
2016-09-21 08:52:30 +0100

    merge engineering and values

--- spec/navi_acceptance_spec.rb
+++ spec/navi_acceptance_spec.rb
@@ -3,8 +3,8 @@ feature "Basic navigation" do
 
   scenario "Go from Values, to CF, to Rates" do
     visit '/'
-    click_link 'Our Values', match: :first
-    expect(page).to have_content 'Our Values Drive Us'
+    click_link 'How We Work', match: :first
+    expect(page).to have_content 'Our Values'
 
     click_link 'Cloud Foundry', match: :first
     expect(page).to have_content 'We offer holistic services'
--- spec/redirect_spec.rb
+++ spec/redirect_spec.rb
@@ -2,5 +2,15 @@ feature "/bad" do
   scenario "gets redirected to Brain Aligned Delivery" do
     visit '/bad'
     expect(page).to have_content 'Brain-Aligned Delivery'
-  end  
+  end
+
+  scenario 'Values redirects to How We Work' do
+    visit '/business-values.html'
+    expect(page).to have_content 'Our values'
+  end
+
+  scenario 'Engineer redirects to How We Work' do
+    visit '/engineering-values.html'
+    expect(page).to have_content 'Our engineering'
+  end
 end
