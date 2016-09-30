4ec622c62e2e610597f953dbd719c869314b9903
Daniel Jones <daniel.jones@engineerbetter.com>
2016-09-30 11:10:42 +0100

    show vat number

--- spec/smoketest_spec.rb
+++ spec/smoketest_spec.rb
@@ -1,8 +1,12 @@
 
 feature "Visit Homepage" do
+  let(:company_number) { '10141478' }
+  let(:vat_number) { 'GB244944977' }
 
   scenario "default view" do
     visit '/'
     expect(page).to have_content 'THE UK CLOUD FOUNDRY SPECIALISTS'
+    expect(page).to have_content company_number
+    expect(page).to have_content vat_number
   end
 end
