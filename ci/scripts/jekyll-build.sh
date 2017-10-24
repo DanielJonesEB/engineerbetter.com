f59571523b49b5c74a8f1b3fe651fabd78f57151
Daniel Jones <daniel.jones@engineerbetter.com>
2017-10-24 11:33:10 +0100

    remove non-blog stuff from CI'

--- ci/scripts/jekyll-build.sh
+++ ci/scripts/jekyll-build.sh
@@ -5,5 +5,5 @@ set -e -x
 pushd engineerbetter.com
   bundle install
   bundle exec jekyll build
-  tar -czf ../jekyll-built/engineerbetter-jekyll-built.tar.gz .
+  tar -czf ../jekyll-built/engineerbetter-blog-built.tar.gz .
 popd
--- ci/scripts/smoke-test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e -x
-
-pushd jekyll-built
-  bundle install
-  xvfb-run -a bundle exec rspec
-popd
