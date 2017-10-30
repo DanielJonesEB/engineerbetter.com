0c9cfb21ee16b73863bb8c92fc8781832d8fdad4
Daniel Jones <daniel.jones@engineerbetter.com>
2017-10-30 14:16:12 +0000

    try out concource task caches

--- ci/scripts/jekyll-build.sh
+++ ci/scripts/jekyll-build.sh
@@ -3,7 +3,7 @@
 set -e -x
 
 pushd engineerbetter.com
-  bundle install
+  bundle install --path ../gem-cache
   bundle exec jekyll build
   tar -czf ../jekyll-built/engineerbetter-blog-built.tar.gz .
   cp -r _site/* ../generated-blog
